69b85274a7d0969b13e19d4ae1e60308e8c5066a
Айса Буваева <akbuvaeva@edu.hse.ru>
2025-04-28 00:20:45 +0300

    test14

--- collect_files.sh
+++ collect_files.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
 
+input="$1"
+output="$2"
 
-input=$1
-output=$2
-if if["$3" = "--max_depth"]; then
-    max_depth=$4
-fi
-
-if[ -z "$max_depth"]; then
-    find "$input" -type f | while read file; do
-        name = $(basename "$file")
-        cp "$file" "$output/$name"
-    done
-else
-    find "$input" -maxdepth "$max_depth" -type f | while read file; do
-        name = $(basename "$file")
-        cp "$file" "$output/$name"
-    done
-fi
+find "$input" -type f | while read file; do
+    name = $(basename "$file")
+    cp "$file" "$output/$name"
+done
