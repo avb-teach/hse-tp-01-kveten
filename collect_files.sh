7a645542e3664b769a26c6b8757946821db78e8f
Айса Буваева <akbuvaeva@edu.hse.ru>
2025-04-27 23:42:12 +0300

    test10

--- collect_files.sh
+++ collect_files.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 if["$1" = "--max_depth"]; then
-    max_depth="$2"
-    input="$3"
-    output="$4"
+    max_depth=$2
+    input=$3
+    output=$4
 else
-    input="$1"
-    output="$2"
+    input=$1
+    output=$2
 fi
 
 if[-z "$max_depth"]; then
